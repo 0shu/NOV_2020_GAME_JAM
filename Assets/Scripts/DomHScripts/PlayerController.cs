eee2669c5354777e4e68868014777bff0494a019
Dominic Hayes <smart_arse000@hotmail.com>
2020-11-07 10:56:08 +0000

    A start on interactables
    Base Interactable class - Inherit this to create interactable objects

---
+++ Assets/Scripts/DomHScripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    List<Interactable> m_InteractablesList = new List<Interactable>();
+
+    #region Unity's example
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+
+
+        ////NOT UNITY'S EXAMPLE
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < m_InteractablesList.Count; ++i)
+                m_InteractablesList[i].Interact();
+        }
+    }
+
+    #endregion Unity's example
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Interactable interactable = other.gameObject.GetComponent<Interactable>();
+        if (interactable != null)
+        {
+            if (m_InteractablesList.Contains(interactable) == false)
+                m_InteractablesList.Add(interactable);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Interactable interactable = other.gameObject.GetComponent<Interactable>();
+        if (interactable != null)
+        {
+            if (m_InteractablesList.Contains(interactable) == true)
+                m_InteractablesList.Remove(interactable);
+        }
+    }
+}
