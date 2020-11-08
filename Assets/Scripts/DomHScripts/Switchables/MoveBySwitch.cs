7a7032e2c7c45bc69d5a6fc6e7b635b9e47095a5
Dominic Hayes <smart_arse000@hotmail.com>
2020-11-08 20:04:20 +0000

    Lever Prefab created
    A start on moving an object using a switch

---
+++ Assets/Scripts/DomHScripts/Switchables/MoveBySwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBySwitch : SwitchableVoid
+{
+    bool m_bAtStartPos = true;
+    Vector3 m_StartPosition;
+    public Vector3 m_Translation = new Vector3();
+    public float m_fMoveSpeed = 0f;
+
+    private void Start()
+    {
+        m_StartPosition = transform.position;
+    }
+
+    protected override void DoAction()
+    {
+        Debug.Log("MoveBySwitch");
+        m_bAtStartPos = !m_bAtStartPos;
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_bAtStartPos) //should get to start pos
+        {
+            transform.position = Vector3.Lerp(transform.position, m_StartPosition, m_fMoveSpeed * Time.deltaTime);
+        }
+        else //should get to translated pos
+        {
+            transform.position = Vector3.Lerp(transform.position, m_StartPosition + m_Translation, m_fMoveSpeed * Time.deltaTime);
+        }
+    }
+}
