4bf43ac881402714c428bf0c8632b2d56793db75
Dominic Hayes <smart_arse000@hotmail.com>
2020-11-11 16:41:22 +0000

    Revert "Added magnetic switch"
    This reverts commit 9c94fc0aeb76afbb2420e42b5a8728878d3a801b.

--- Assets/Scripts/DomHScripts/Switchables/MagnetSwitcherObject.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(Magnetic))]
-public class MagnetSwitcherObject : MonoBehaviour { }
--- Assets/Scripts/DomHScripts/Switchables/Switch.cs
+++ Assets/Scripts/DomHScripts/Switchables/Switch.cs
@@ -9,8 +9,8 @@ public class SwitchEventBool : UnityEvent<bool> { }
 
 public class Switch : Interactable
 {
-    private bool m_bSwitchStatus = false;
-    
+    bool m_bSwitchStatus = false;
+
     private SwitchEventVoid m_SwitchEventVoid;
     private SwitchEventBool m_SwitchEventBool;
 
